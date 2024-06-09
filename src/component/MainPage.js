08a6ca463b0c7708cf72a9e2f2d82779efaf7339
passavit <pm@hirequalitysoftware.com>
2024-06-09 23:16:35 +0700

    upload profile pic

--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -2,63 +2,64 @@ import React, { useState, useEffect } from "react";
 import Header from "./Header/Header";
 import SocialLinks from "./SocialLinks/SocialLinks";
 import ProductCard from "./ProductCard/ProductCard";
-import CreateUserModal from './NewUserModal/CreateUserModal';
+import CreateUserModal from "./NewUserModal/CreateUserModal";
 import "./MainPage.css";
 import { auth, provider } from "../firebaseConfig";
 import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import { useUser } from '../Context/UserContext';
-import userService from '../services/userService';
-import productService from '../services/productService';
+import { useUser } from "../Context/UserContext";
+import userService from "../services/userService";
+import productService from "../services/productService";
 
 const MainPage = () => {
   const [profileData, setProfileData] = useState({
-    profileImage: '',
-    profileName: '',
-    igUrl: '',
-    tiktokUrl: '',
+    profileImage: "",
+    profileName: "",
+    igUrl: "",
+    tiktokUrl: "",
   });
 
   const [products, setProducts] = useState([]);
   const [showModal, setShowModal] = useState(false);
-  const [uid, setUid] = useState('');
+  const [uid, setUid] = useState("");
   const { user, setUser } = useUser(); // Use user context
   const navigate = useNavigate();
 
   useEffect(() => {
-    const storedUser = sessionStorage.getItem('user');
+    const storedUser = sessionStorage.getItem("user");
     if (storedUser) {
       setUser(JSON.parse(storedUser));
     }
   }, [setUser]);
 
   useEffect(() => {
-    const fetchProfileData = async () => {
+    const fetchProfileData = async (profileId) => {
       try {
-        const response = await userService.getUserByUid(process.env.REACT_APP_USER_ID);
+        const response = await userService.getProfileById(profileId);
         const data = response.data;
-
         setProfileData({
-          profileImage: data.profileImageUrl || '',
-          profileName: data.name || '',
-          igUrl: data.igUrl || 'https://instagram.com',
-          tiktokUrl: data.tiktokUrl || 'https://tiktok.com',
+          profileImage: data.profileImageUrl || "",
+          profileName: data.name || "",
+          igUrl: data.igUrl || "https://instagram.com",
+          tiktokUrl: data.tiktokUrl || "https://tiktok.com",
         });
       } catch (error) {
-        console.error('Error fetching profile data:', error);
+        console.error("Error fetching profile data:", error);
       }
     };
 
     const fetchProducts = async () => {
       try {
-        const response = await productService.getProductsByProfileId(process.env.REACT_APP_USER_ID);
+        const response = await productService.getProductsByProfileId(
+          process.env.REACT_APP_USER_ID
+        );
         setProducts(response.data);
       } catch (error) {
-        console.error('Error fetching products:', error);
+        console.error("Error fetching products:", error);
       }
     };
 
-    fetchProfileData();
+    fetchProfileData(process.env.REACT_APP_USER_ID);
     fetchProducts();
   }, []);
 
@@ -68,10 +69,10 @@ const MainPage = () => {
       const user = result.user;
       setUid(user.uid);
       setUser(user);
-      sessionStorage.setItem('user', JSON.stringify(user));
+      sessionStorage.setItem("user", JSON.stringify(user));
       checkUserExists(user.uid);
     } catch (error) {
-      console.error('Error signing in with Google', error);
+      console.error("Error signing in with Google", error);
     }
   };
 
@@ -79,12 +80,12 @@ const MainPage = () => {
     try {
       const response = await userService.getUserByUid(uid);
       if (response.data) {
-        navigate('/Tiktok_Web/admin'); // Navigate to admin page if user exists
+        navigate("/Tiktok_Web/admin"); // Navigate to admin page if user exists
       } else {
         setShowModal(true); // Show modal to create user if not exists
       }
     } catch (error) {
-      console.error('Error checking user existence:', error);
+      console.error("Error checking user existence:", error);
       setShowModal(true); // Show modal to create user if error occurs
     }
   };
@@ -97,14 +98,20 @@ const MainPage = () => {
       setShowModal(false);
       navigate(`/Tiktok_Web/admin`); // Navigate to admin page after creating user
     } catch (error) {
-      console.error('Error creating user:', error);
+      console.error("Error creating user:", error);
     }
   };
 
   return (
     <div className="main-page">
-      <Header profileImage={profileData.profileImage} profileName={profileData.profileName} />
-      <SocialLinks igUrl={profileData.igUrl} tiktokUrl={profileData.tiktokUrl} />
+      <Header
+        profileImage={profileData.profileImage}
+        profileName={profileData.profileName}
+      />
+      <SocialLinks
+        igUrl={profileData.igUrl}
+        tiktokUrl={profileData.tiktokUrl}
+      />
       <div className="product-grid">
         {products.map((product, index) => (
           <ProductCard
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,14 +1,16 @@
 // src/services/userService.js
-import axios from 'axios';
+import axios from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
 const userService = {
   getUserByUid: (uid) => axios.get(`${API_URL}/users/uid/${uid}`),
+  getProfileById: (id) => axios.get(`${API_URL}/profiles/${id}`),
   createUser: (user) => axios.post(`${API_URL}/users`, user),
   updateUser: (id, user) => axios.put(`${API_URL}/users/${id}`, user),
   deleteUser: (id) => axios.delete(`${API_URL}/users/${id}`),
-  updateProfile: (profileId, profile) => axios.put(`${API_URL}/profiles/${profileId}`, profile) 
+  updateProfile: (profileId, profile) =>
+    axios.put(`${API_URL}/profiles/${profileId}`, profile),
 };
 
 export default userService;

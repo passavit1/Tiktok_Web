ce6663e6741986fe21b4a76e025cd5ffb7ea0cee
passavit <pm@hirequalitysoftware.com>
2024-06-15 11:57:34 +0700

    Change IG link

--- src/component/SocialLinks/SocialLinks.js
+++ src/component/SocialLinks/SocialLinks.js
@@ -5,10 +5,20 @@ import './SocialLinks.css';
 const SocialLinks = ({ igUrl, tiktokUrl }) => {
   return (
     <div className="social-links">
-      <a href={igUrl} target="_blank" rel="noopener noreferrer">
+      <a
+        href={igUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+        data-title="Instagram"
+        data-html={igUrl}
+        data-id="uniqueId"
+        data-kid="uniqueKid"
+        data-atype="4"
+        data-type="4"
+      >
         <FaInstagram />
       </a>
-      <a href={tiktokUrl} target="_blank" rel="noopener noreferrer">
+      <a href={tiktokUrl} target="_blank" rel="noopener noreferrer" style={{pointerEvents:"unset"}}>
         <FaTiktok />
       </a>
     </div>
